440fa1879bfddcde1f00364cb9fa17f57767fa5e
pedone1 <e.senshu@gmail.com>
2018-11-21 23:48:57 +0100

    basic localization

---
+++ KFZ-Konfigurator/KFZ-Konfigurator/Resources/Localization/LocalizationManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Resources;
+using System.Web;
+
+namespace KFZ_Konfigurator.Resources.Localization
+{
+    public static class LocalizationManager
+    {
+        public static string Localize(string id)
+        {
+            return GetCurrentCultureResourceManager().GetString(id) ?? throw new ArgumentException($"Resource with the name {id} was not found");
+        }
+
+        public static string LocalizeEnum<T>(string item) where T : struct, IConvertible
+        {
+            if (!typeof(T).IsEnum)
+                throw new ArgumentException("Type of T must be an enum");
+
+            return Localize($"{typeof(T).Name}_{item}");
+        }
+
+        private static ResourceManager GetCurrentCultureResourceManager()
+        {
+            switch (CultureInfo.CurrentCulture.Name)
+            {
+                case "en-US":
+                    return en_US.ResourceManager;
+                default:
+                    return en_US.ResourceManager;
+            }
+        }
+    }
+}
