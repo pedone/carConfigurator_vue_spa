fbe89f811623191e4929cb066ef6a7a222502dbb
pedone1 <e.senshu@gmail.com>
2018-11-30 02:36:36 +0100

    moved functions to viewmodel

--- KFZ-Konfigurator/KFZ-Konfigurator/Scripts/app/ConfigurationOverviewViewModel.js
+++ KFZ-Konfigurator/KFZ-Konfigurator/Scripts/app/ConfigurationOverviewViewModel.js
@@ -1,11 +1,57 @@
 ﻿class ConfigurationOverviewViewModel {
     /**
      * @param {string} configurationUrl
+     * @param {HandlerOptions} handlerOptions
      */
-    constructor(configurationUrl) {
+    constructor(configurationUrl, handlerOptions) {
+        self = this;
         /** @type {string} */
         this.configurationLinkUrl = ko.observable(configurationUrl || "");
         /** @type {string} */
         this.configurationName = ko.observable("");
+        /** @type {HandlerOptions} */
+        this.options = handlerOptions;
+    }
+
+    generateConfigurationLink() {
+        $.ajax({
+            type: 'POST',
+            url: '/ConfigurationOverview/GenerateConfigurationLink',
+            data: { __RequestVerificationToken: self.options.antiForgeryToken, configurationName: self.configurationName() },
+            contentType: 'application/x-www-form-urlencoded',
+        }).done(function (data) {
+            self.configurationLinkUrl(data);
+        }).fail(function (error) {
+            console.log('failed to generate configuration link: ' + error.responseText + ' (' + error.statusText + ')');
+        });
+    }
+
+    placeOrder() {
+        $.ajax({
+            type: 'POST',
+            url: '/ConfigurationOverview/PlaceOrder',
+            data: { __RequestVerificationToken: self.options.antiForgeryToken, configurationName: self.configurationName() },
+            contentType: 'application/x-www-form-urlencoded',
+            dataType: 'text'
+        }).done(function (data) {
+            self.configurationLinkUrl(data);
+            if (self.options.placeOrderSuccess) {
+                self.options.placeOrderSuccess();
+            }
+        })
+        .fail(
+            function (error) {
+                console.log('failed to place order: ' + error.responseText + ' (' + error.statusText + ')');
+                if (self.options.placeOrderFailure) {
+                    self.options.placeOrderFailure();
+                }
+        });
     }
 }
+
+/**
+ * @typedef {Object} HandlerOptions
+ * @property {string} antiForgeryToken
+ * @property {Function|null} placeOrderSuccess
+ * @property {Function|null} placeOrderFailure
+ */
