3be14951f660c6889364900f83718f284f32988f
pedone <salat.kopp@gmail.com>
2018-12-17 23:28:01 +0100

    gulp added with tasks

--- KFZ-Konfigurator/KFZ-Konfigurator/App_Start/BundleConfig.cs
+++ KFZ-Konfigurator/KFZ-Konfigurator/App_Start/BundleConfig.cs
@@ -8,24 +8,24 @@ namespace KFZ_Konfigurator
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            //            "~/Scripts/jquery-{version}.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            //// Use the development version of Modernizr to develop with and learn from. Then, when you're
+            //// ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrapVue").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/knockout-{version}.js",
-                      "~/Scripts/vue.js",
-                      "~/Scripts/underscore-{version}.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrapVue").Include(
+            //          "~/Scripts/bootstrap.js",
+            //          "~/Scripts/knockout-{version}.js",
+            //          "~/Scripts/vue.js",
+            //          "~/Scripts/underscore-{version}.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/app/styles.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css",
+            //          "~/Content/app/styles.css"));
         }
     }
 }
---
+++ KFZ-Konfigurator/KFZ-Konfigurator/gulpfile.js
@@ -0,0 +1,68 @@
+﻿'use strict';
+
+const { src, dest, series } = require('gulp'),
+    rimraf = require('rimraf'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    babel = require('gulp-babel'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename');
+
+const paths = {
+    less: {
+        src: './Content/**/*.less',
+        destDir: './Content',
+    },
+    css: {
+        src: ['./Content/**/*.css', '!./Content/**/*.min.css'],
+        dest: './Content/out/site.min.css'
+    },
+    babel: {
+        src: ['./Scripts/**/*.js', '!./Scripts/**/*.min.js'],
+        dest: './Scripts/out/site.min.js'
+    }
+};
+
+// LESS
+
+function compileLess() {
+    return src(paths.less.src)
+        .pipe(less())
+        .pipe(rename({ extension: '.css' }))
+        .pipe(dest(paths.less.destDir));
+}
+
+// CSS
+
+function cleanCSS(done) {
+    rimraf(paths.css.dest, done);
+}
+
+function minCSS() {
+    return src(paths.css.src)
+        .pipe(concat(paths.css.dest))
+        .pipe(cssmin())
+        .pipe(dest('.'));
+}
+
+// JS
+
+function cleanJS(done) {
+    rimraf(paths.babel.dest, done);
+}
+
+function bundleMinJS() {
+    return src(paths.babel.src)
+        .pipe(babel({ presets: ['@babel/env'] }))
+        .pipe(concat(paths.babel.dest))
+        .pipe(uglify())
+        .pipe(dest('.'));
+}
+
+exports.cleanCSS = cleanCSS;
+exports.minCSS = series(compileLess, minCSS);
+
+exports.compileLess = compileLess;
+exports.cleanJS = cleanJS;
+exports.bundleJS = series(cleanJS, bundleMinJS);
