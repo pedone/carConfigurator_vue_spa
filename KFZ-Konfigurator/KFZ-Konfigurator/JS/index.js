d0f56ec5b836880c117c4930f7d1a427c5667700
pedone <salat.kopp@gmail.com>
2018-12-30 18:03:04 +0100

    basic webpack config and app component

---
+++ KFZ-Konfigurator/KFZ-Konfigurator/JS/index.js
@@ -0,0 +1,12 @@
+﻿import Vue from 'vue';
+import App from './App';
+
+Vue.config.productionTip = false;
+
+new Vue({
+    el: '#app',
+    components: {
+        App
+    },
+    render: h => h(App)
+});
---
+++ KFZ-Konfigurator/KFZ-Konfigurator/webpack.config.js
@@ -0,0 +1,39 @@
+﻿const webpack = require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const bundleOutputDir = './JS/dist';
+
+const config = {
+    entry: './JS/index.js',
+    watch: true,
+  output: {
+    path: path.resolve(__dirname, bundleOutputDir),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue'
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new LodashModuleReplacementPlugin
+  ]
+}
+
+module.exports = config;
